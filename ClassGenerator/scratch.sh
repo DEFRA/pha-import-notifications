e7d6c92d6fc9d4117cd0983955d3b736b0c1a2fa
Daniel Klemm <dklemm@github.com>
2024-11-18 09:30:05 +0000

    PHAIN-49 : Remove class generator POC

--- ClassGenerator/scratch.sh
+++
@@ -1,6 +0,0 @@
-
-#curl -o cdms-public-openapi-v0.1.json  http://localhost:5000/swagger/public-v0.1/swagger.json
-#Nswag
-#cdms 
-#npx @openapitools/openapi-generator-cli generate -i cdms-public-openapi-v0.1.json -g csharp -o ./clients/cdms
-#openapi-generator generate -i cdms-public-openapi-v0.1.json -g csharp -o ./clients/cdms --skip-validate-spec 
