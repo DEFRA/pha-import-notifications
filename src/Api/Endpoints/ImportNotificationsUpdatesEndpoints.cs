f37ccb9341e020c306af453307631b4643aa1dbc
Tim Peel <tim.peel@equalexperts.com>
2024-11-24 20:35:09 +0000

    Add default values for the updates request

--- src/Api/Endpoints/ImportNotificationsUpdatesEndpoints.cs
+++ src/Api/Endpoints/ImportNotificationsUpdatesEndpoints.cs
@@ -50,11 +50,13 @@ public static class ImportNotificationsUpdatesEndpoints
     // ReSharper disable once ClassNeverInstantiated.Local
     private sealed record UpdatedImportNotificationRequest
     {
-        [Description("Allows a specific page to be requested, default is 1")]
+        [Description("Allows a specific page to be requested")]
+        [DefaultValue(1)]
         [FromQuery(Name = "page")]
         public int? Page { get; init; } = 1;
 
-        [Description("Allows a page size to be requested, default is 100")]
+        [Description("Allows a page size to be requested")]
+        [DefaultValue(100)]
         [FromQuery(Name = "pageSize")]
         public int? PageSize { get; init; } = 100;
 
@@ -67,7 +69,8 @@ public static class ImportNotificationsUpdatesEndpoints
                 + "time of request. If the time period between from and to is greater than 24 hours then "
                 + "the request will be invalid."
         )]
+        [DefaultValue(typeof(DateTime), "Time of execution")] // This doesn't add anything to the spec - do we need it?
         [FromQuery(Name = "to")]
-        public DateTime? To { get; init; }
+        public DateTime? To { get; init; } = DateTime.Now;
     }
 }
