982100a0ff0c83ecaf357421c462a5696a1041b4
Phil Morgan <phil@psmorgan.co.uk>
2024-12-02 14:54:48 +0000

    PHAIN-80: Add Basic auth for BTMS
    BTMS currently authenticates via basic auth.  This change adds a basic auth header to all requests, configured by environment variables.

--- src/Api/JsonApi/ServiceCollectionExtensions.cs
+++ src/Api/JsonApi/ServiceCollectionExtensions.cs
@@ -7,7 +7,8 @@ public static class ServiceCollectionExtensions
 {
     public static IHttpClientBuilder AddJsonApiClient(
         this IServiceCollection services,
-        Func<IServiceProvider, string> baseUrlFactory
+        Func<IServiceProvider, string> baseUrlFactory,
+        Func<IServiceProvider, string> basicAuthCredentialFactory
     )
     {
         return services.AddHttpClient<JsonApiClient>(
@@ -16,6 +17,10 @@ public static class ServiceCollectionExtensions
                 httpClient.BaseAddress = new Uri(baseUrlFactory(sp));
                 httpClient.DefaultRequestHeaders.Accept.Add(
                     new MediaTypeWithQualityHeaderValue("application/vnd.api+json")
+                );
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                    "Basic",
+                    basicAuthCredentialFactory(sp)
                 );
 
                 if (httpClient.BaseAddress.Scheme.Equals("https", StringComparison.OrdinalIgnoreCase))
---
+++ tests/Api.Tests/Configuration/BtmsOptionsTests.cs
@@ -0,0 +1,20 @@
+using Defra.PhaImportNotifications.Api.Configuration;
+using FluentAssertions;
+
+namespace Defra.PhaImportNotifications.Api.Tests.Configuration;
+
+public class BtmsOptionsTests
+{
+    [Fact]
+    public void BasicAuthCredential_ReturnsEncodedCredentials()
+    {
+        var btmsOptions = new BtmsOptions
+        {
+            BaseUrl = "https://test",
+            Password = "password",
+            Username = "username",
+        };
+
+        btmsOptions.BasicAuthCredential.Should().Be("dXNlcm5hbWU6cGFzc3dvcmQ=");
+    }
+}
