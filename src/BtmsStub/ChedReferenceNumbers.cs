7855600d904c2c00312bde645b1b5f89a7eef810
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-12-20 12:43:40 +0000

    PHAIN-38: Update BTMS stub with additional import notifications (CHEDA/D/P/PP)

--- src/BtmsStub/ChedReferenceNumbers.cs
+++ src/BtmsStub/ChedReferenceNumbers.cs
@@ -2,5 +2,8 @@ namespace Defra.PhaImportNotifications.BtmsStub;
 
 public static class ChedReferenceNumbers
 {
-    public const string ChedA = "CHEDA.GB.2024.1234567";
+    public const string ChedA = "CHEDA.GB.2024.4792831";
+    public const string ChedD = "CHEDD.GB.2024.5019877";
+    public const string ChedP = "CHEDP.GB.2024.4144842";
+    public const string ChedPP = "CHEDPP.GB.2024.3726460";
 }
