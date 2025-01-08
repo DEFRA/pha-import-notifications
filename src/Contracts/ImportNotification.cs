1777f55bbb8f14c0884af39da050f7b3582601f2
d-ee-klemm <184640818+d-ee-klemm@users.noreply.github.com>
2025-01-08 16:19:41 +0000

    PHAIN-72 : Publish customs decisions with notifications (#104)
    * PHAIN-72 : Publish customs decisions with notifications
    
    * PHAIN-72 : Publish customs decisions with notifications

--- src/Contracts/ImportNotification.cs
+++ src/Contracts/ImportNotification.cs
@@ -3,4 +3,6 @@ namespace Defra.PhaImportNotifications.Contracts;
 public partial record ImportNotification
 {
     public IReadOnlyList<CustomsClearanceRequest>? ClearanceRequests { get; init; }
+
+    public IReadOnlyList<CustomsClearanceRequest>? ClearanceDecisions { get; init; }
 }
