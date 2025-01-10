04b4582f70a7b486147d10f7395f2e1786edbf8f
Tim Peel <tim.peel@equalexperts.com>
2025-01-10 13:07:01 +0000

    Snapshot UTC now so execution time does not affect value being used

--- tests/Api.Tests/Endpoints/Validation/DateTimeRangeIsLessThanOrEqualToValidatorTests.cs
+++ tests/Api.Tests/Endpoints/Validation/DateTimeRangeIsLessThanOrEqualToValidatorTests.cs
@@ -4,10 +4,12 @@ namespace Defra.PhaImportNotifications.Api.Tests.Endpoints.Validation;
 
 public class DateTimeRangeIsLessThanOrEqualToValidatorTests
 {
+    private static readonly DateTime s_utcNow = DateTime.UtcNow;
+
     public static TheoryData<TimeRange, TimeSpan> DateTimeRangeDoesntExceedTimespan = new()
     {
-        { new TimeRange(DateTime.UtcNow, DateTime.UtcNow.Add(TimeSpan.FromMinutes(29))), TimeSpan.FromMinutes(30) },
-        { new TimeRange(DateTime.UtcNow, DateTime.UtcNow.Add(TimeSpan.FromMinutes(30))), TimeSpan.FromMinutes(30) },
+        { new TimeRange(s_utcNow, s_utcNow.Add(TimeSpan.FromMinutes(29))), TimeSpan.FromMinutes(30) },
+        { new TimeRange(s_utcNow, s_utcNow.Add(TimeSpan.FromMinutes(30))), TimeSpan.FromMinutes(30) },
     };
 
     [Theory, MemberData(nameof(DateTimeRangeDoesntExceedTimespan))]
@@ -17,10 +19,7 @@ public class DateTimeRangeIsLessThanOrEqualToValidatorTests
     public static TheoryData<TimeRange, TimeSpan> DateTimeRangeExceedTimespan = new()
     {
         {
-            new TimeRange(
-                DateTime.UtcNow,
-                DateTime.UtcNow.Add(TimeSpan.FromMinutes(30).Add(TimeSpan.FromMilliseconds(1)))
-            ),
+            new TimeRange(s_utcNow, s_utcNow.Add(TimeSpan.FromMinutes(30).Add(TimeSpan.FromMilliseconds(1)))),
             TimeSpan.FromMinutes(30)
         },
     };
