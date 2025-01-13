ba817a8f41a19735c084440e41a38230ebbfa6d8
Phil Morgan <phil@psmorgan.co.uk>
2025-01-13 15:40:55 +0000

    PHAIN-104: Disable BTMS stub in more environments (#114)
    The BTMS stub has been moved to its own service.

--- src/BtmsStub/Configuration/BtmsStubOptions.cs
+++ src/BtmsStub/Configuration/BtmsStubOptions.cs
@@ -3,5 +3,5 @@ namespace Defra.PhaImportNotifications.BtmsStub.Configuration;
 public class BtmsStubOptions
 {
     public int Port { get; init; } = 8090;
-    public bool Enabled { get; init; }
+    public bool Enabled { get; init; } = false;
 }
