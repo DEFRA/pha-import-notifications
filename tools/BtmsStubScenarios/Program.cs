ee82227c530b05870275d359115112c4da695f16
Tim Peel <tim.peel@equalexperts.com>
2025-01-20 16:32:40 +0000

    Ensure CDS data is not dropped

--- tools/BtmsStubScenarios/Program.cs
+++ tools/BtmsStubScenarios/Program.cs
@@ -5,6 +5,7 @@
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 // ReSharper disable InconsistentNaming
 
@@ -30,6 +31,10 @@ var cheds = new[]
     "CHEDD.GB.2024.5019877",
     "CHEDP.GB.2024.4144842",
     "CHEDPP.GB.2024.3726460",
+    // The following CHED shows the relationship JSON structure
+    // we need to follow so if it changes, the logic for fake
+    // movements also needs to change below
+    "CHEDA.GB.2024.5149729",
 };
 
 foreach (var ched in cheds)
@@ -41,6 +46,64 @@ foreach (var ched in cheds)
     json = json.Replace("GBLGW4", "GBTEEP1");
     json = json.Replace("GBLGP1", "GBTEEP1");
     json = json.Replace("GBHCH4PP", "GBTEEP1");
+
+    // Fake movement relationships for CHED
+    if (ched == "CHEDA.GB.2024.5129502")
+    {
+        var document = JsonNode.Parse(json);
+        if (document is null)
+            throw new InvalidOperationException("Failed to parse JSON");
+        var dataDocument = document["data"]?.AsObject();
+        if (dataDocument is null)
+            throw new InvalidOperationException("Failed to get data as object");
+        if (!dataDocument.ContainsKey("relationships"))
+            throw new InvalidOperationException("Failed to find relationships in data");
+
+        dataDocument["relationships"] = new JsonObject
+        {
+            ["movements"] = new JsonObject
+            {
+                ["links"] = new JsonObject
+                {
+                    ["self"] = "/api/import-notifications/CHEDA.GB.2024.5129502",
+                    ["related"] = "/api/import-notifications/CHEDA.GB.2024.5129502/movements",
+                },
+                ["data"] = new JsonArray
+                {
+                    new JsonObject
+                    {
+                        ["type"] = "movements",
+                        ["id"] = "24GBC1IQDD278IZAR8",
+                        ["meta"] = new JsonObject
+                        {
+                            ["matched"] = true,
+                            ["sourceItem"] = 1,
+                            ["destinationItem"] = 1,
+                            ["matchingLevel"] = 1,
+                            ["self"] = "/api/movements/24GBC1IQDD278IZAR8/import-notifications",
+                        },
+                    },
+                    new JsonObject
+                    {
+                        ["type"] = "movements",
+                        ["id"] = "24GBCND8RONCFGAAR3",
+                        ["meta"] = new JsonObject
+                        {
+                            ["matched"] = true,
+                            ["sourceItem"] = 1,
+                            ["destinationItem"] = 1,
+                            ["matchingLevel"] = 1,
+                            ["self"] = "/api/movements/24GBCND8RONCFGAAR3/import-notifications",
+                        },
+                    },
+                },
+            },
+        };
+
+#pragma warning disable CA1869
+        json = JsonSerializer.Serialize(document, new JsonSerializerOptions { WriteIndented = true });
+#pragma warning restore CA1869
+    }
 
     await File.WriteAllTextAsync($"{outputPath}btms-import-notification-single-{ched}.json", json);
 }
