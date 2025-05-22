35d836c4ba8825d8badf562773240ebef10c08b2
Darren Walker <9861667+FluentSynergyDW@users.noreply.github.com>
2025-05-22 11:43:58 +0100

    Remove Hello World endpoint

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,7 +1,6 @@
 using System.Reflection;
 using System.Text;
 using Defra.PhaImportNotifications.Api.Configuration;
-using Defra.PhaImportNotifications.Api.Endpoints;
 using Defra.PhaImportNotifications.Api.Endpoints.ImportNotifications;
 using Defra.PhaImportNotifications.Api.Extensions;
 using Defra.PhaImportNotifications.Api.JsonApi;
@@ -186,7 +185,6 @@ static WebApplication BuildWebApplication(WebApplicationBuilder builder)
     app.UseAuthorization();
 
     app.MapHealthChecks("/health");
-    app.MapExampleEndpoints();
     app.MapImportNotificationsEndpoints();
 
     app.UseSwagger(options =>
